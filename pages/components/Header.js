c656af4c5fb0577daf49cbe3c69e956da796376e
Owoade <63651250+Owoade@users.noreply.github.com>
2021-12-23 12:48:14 +0100

    Update Header.js

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     return ( 
         <header>
         <div className="container flex space-between">
-            <p className="logo"><img src="img/logo.jpeg" style={{width:"200px"}} /></p>
+            <p className="logo">Viralclicks<span>-mine</span></p>
             <nav>
                 <a href="/">Home</a>
                 <a href="/Our-Packages">Our Packages</a>
